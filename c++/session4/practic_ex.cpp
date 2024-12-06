11fe3364e6bd95c6a757ef03f907b368ec3267e3
Dheeraj Kumar <dk8549644@gmail.com>
2024-12-06 11:35:01 +0530

    add #1 practice

---
+++ c++/session4/practic_ex.cpp
@@ -0,0 +1,30 @@
+// c++ program to find the array size
+
+/*
+#include<iostream>
+using namespace std;
+
+int main(){
+  int arr[10]; // declare an array of size 10
+
+  // int size = sizeof(arr)/sizeof(arr[0]); // find the size of the array
+
+  int size = sizeof(arr)/sizeof(int);
+  cout<<"Size of the array is: "<<size<<endl;
+  return 0;
+}
+*/
+
+
+// ++++++++++++ output ++++++++++++++++++
+
+// PS J:\My Drive\DSA-WORDSPACE\c++\session4> g++ practic_ex.cpp -o practic_ex
+// PS J:\My Drive\DSA-WORDSPACE\c++\session4> ./practic_ex
+// Size of the array is: 10
+// PS J:\My Drive\DSA-WORDSPACE\c++\session4>
+
+
+
+// ---------------------------------
+
+// #2. Write program to check if two arrays are the same or not. [Asked in: Amazon, goldman Sachs]
