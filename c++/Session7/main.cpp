644f76793aa1efcae8364e88eab7cc7434b55c74
Dheeraj Kumar <dk8549644@gmail.com>
2025-01-03 22:12:22 +0530

    add binary tree traversal implementations in main.cpp

---
+++ c++/Session7/main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+    Node(int data){
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+void preorder(struct Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    cout << root->data << " -> ";
+    preorder(root->left);
+    preorder(root -> right);
+}
+
+void postorder(struct Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " -> ";
+}
+
+void inorder(struct Node* root){
+    if(root == NULL){
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " -> ";
+    inorder(root->right);
+}
+
+int main(){
+    struct Node* root = new Node(1);
+    root->left = new Node(12);
+    root->right = new Node(9);
+    root->left->left = new Node(5);
+    root->left->right = new Node(30);
+
+    cout << "\n Inorder traversal of binary tree is \n";
+    inorder(root);
+
+    cout << "\n Preorder traversal of binary tree is \n";
+    preorder(root);
+
+    cout << "\n Postorder traversal of binary tree is \n";
+    postorder(root);
+
+    return 0;
+}
+
+
+// --------------- Output ---------------------
+// PS C:\Users\hp\Desktop\TCS IT\DSA-WORDSPACE\c++\Session7> g++ main.cpp -o main
+// PS C:\Users\hp\Desktop\TCS IT\DSA-WORDSPACE\c++\Session7> ./main
+
+//  Inorder traversal of binary tree is 
+// 5 -> 12 -> 30 -> 1 -> 9 ->
+//  Preorder traversal of binary tree is
+// 1 -> 12 -> 5 -> 30 -> 9 ->
+//  Postorder traversal of binary tree is
+// 5 -> 30 -> 12 -> 9 -> 1 ->
+
+
