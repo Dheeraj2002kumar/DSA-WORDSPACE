89f79183b8a30e0816024493b6c9596c314a1b63
Dheeraj Kumar <dk8549644@gmail.com>
2025-01-09 11:28:51 +0530

    add function to print multiplication table for a given number

--- python/Logic_Building/logic.js
+++ python/Logic_Building/logic.js
@@ -30,3 +30,33 @@ function isEven(n){
 
 // +++++++++++++++++++++++++++++++++++++++++++++
 
+
+/*
+
+Given a number n, we need to print its table. 
+
+Examples : 
+
+Input:  5
+Output: 
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+5 * 7 = 35
+5 * 8 = 40
+5 * 9 = 45
+5 * 10 = 50
+
+*/
+
+function printTable(n){
+    let obj = []
+    for (let i = 1; i <= 10; i++){
+        console.log(`${n} * ${i} = ${n*i}`)
+    }
+}
+let n = 5
+printTable(n)
