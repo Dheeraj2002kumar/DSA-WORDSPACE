b1712f19df59db013804f10469c9c540222b8df5
Dheeraj Kumar <dk8549644@gmail.com>
2025-01-09 11:22:43 +0530

    add JavaScript function to check if a number is even or odd

---
+++ python/Logic_Building/logic.js
@@ -0,0 +1,32 @@
+/*
+
+Given a number n, check whether it is even or odd. Return true for even and false for odd.
+
+Examples: 
+
+Input: 2 
+Output: true
+
+
+Input: 5
+Output: false
+
+*/
+
+function isEven(n){
+    if (n % 2 == 0){
+        return "true"
+    } 
+    else{
+        return "false"
+    }
+}
+
+// console.log(isEven(2))
+// console.log(isEven(5))
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++
+
