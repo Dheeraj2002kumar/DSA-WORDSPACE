aa7b15ed79dd9e3c8626772131b6bbb45dd1cc22
Dheeraj Kumar <dk8549644@gmail.com>
2025-03-24 12:38:49 +0530

    add deleteNode and printList methods to CircularDoublyLinkedList implementation in JavaScript

--- python/Linked_list/Types_of_Linked_list/Circular_Doubly_linked_list/circular-doubly-linked-list.js
+++ python/Linked_list/Types_of_Linked_list/Circular_Doubly_linked_list/circular-doubly-linked-list.js
@@ -44,5 +44,50 @@ class  CircularDoublyLinkedList {
         }
     }
 
-    
+    deleteNode(value){
+        if (!this.head) return;
+
+        let current = this.head;
+        do {
+            if (current.data === value){
+                if (current === this.head){
+                    this.head = this.head.next;
+                    this.tail.next = this.head;
+                    this.head.prev = this.tail;
+                } 
+                else if (current === this.tail){
+                    this.tail = this.tail.prev;
+                    this.tail.next = this.head;
+                    this.head.prev = this.tail;
+                }
+                else {
+                    current.prev.next = current.next;
+                    current.next.prev = current.prev;
+                }
+                return;
+            }
+            current = current.next;
+        } while(current !== this.head);
+    }
+
+    printList(){
+        if (!this.head){
+            console.log("List is Empty");
+            return;
+        }
+
+        let current = this.head;
+        let result = "";
+        do{
+            result += current.data + " ⇄ ";
+            current = current.next;
+        } while(current !== this.head);
+        console.log(result + "(HEAD)");
+    }
 }
+
+let cdll = new CircularDoublyLinkedList();
+cdll.insertAtEnd(10);
+cdll.insertAtEnd(20);
+cdll.insertAtBeginning(5);
+cdll.printList();
