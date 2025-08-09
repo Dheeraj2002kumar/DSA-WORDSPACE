a55dc86cd0aa6da8a7627b14cc4cdc61338d0d68
Dheeraj Kumar <dk8549644@gmail.com>
2025-08-09 11:58:47 +0530

    Implement search function for "Search in Rotated Sorted Array"

--- DSA/2_Searching/1_BinarySearch/BinarySearch-Problem/SearchInRotatedSortedArray.cpp
+++ DSA/2_Searching/1_BinarySearch/BinarySearch-Problem/SearchInRotatedSortedArray.cpp
@@ -40,3 +40,27 @@ nums is an ascending array that is possibly rotated.
 
 */
 
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        
+        int start = 0, end = arr.size()-1, mid;
+
+        while(start <= end){
+            mid = start + (end-start) / 2;
+            // find target element
+            if (arr[mid] == target) return mid;
+            // left side sorted
+            else if (arr[mid] >= arr[0]) {
+                if (arr[start] <= target && arr[mid] > target) end = mid-1;
+                else start = mid + 1;
+            }
+            else{
+                if (arr[mid] < target && arr[end] >= target) start = mid + 1;
+                else end = mid -1;
+            }
+        }
+
+        return -1;
+    }
+};
