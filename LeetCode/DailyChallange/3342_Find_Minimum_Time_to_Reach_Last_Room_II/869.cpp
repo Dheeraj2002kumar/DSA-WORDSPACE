4a5a22e6b89b88537f43a0d12e8e695aa1be9e20
Dheeraj Kumar <dk8549644@gmail.com>
2025-08-10 22:02:14 +0530

    Implement solution for "Reordered Power of 2"

--- LeetCode/DailyChallange/3342_Find_Minimum_Time_to_Reach_Last_Room_II/869.cpp
+++ LeetCode/DailyChallange/3342_Find_Minimum_Time_to_Reach_Last_Room_II/869.cpp
@@ -24,3 +24,25 @@ Constraints:
 
 */
 
+class Solution {
+public:
+    string getSortedStr(int n){
+        string s = to_string(n);  // 10 digits // 0(d)
+        sort(begin(s), end(s));  // O(dlogd)
+        return s;
+    }
+    
+    bool reorderedPowerOf2(int n) {
+        string s = getSortedStr(n);
+
+        // check all 2 powers and see if it matches with s
+        // 2^29
+        for (int p = 0; p <= 29; p++){
+            if (s == getSortedStr(1 << p)){
+                return  true;
+            }
+        }
+
+        return false;
+    }
+};
