caa7356d24bf1e28a10ee6734439a0a9a3fdf118
Dheeraj Kumar <dk8549644@gmail.com>
2025-08-15 00:10:28 +0530

    add leetcode 2264 problem

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    int d;
+    printf("Enter your age: ");
+    scanf("%d", &age); // Reads an integer and stores it in 'age'
+    printf("You are %d years old.", age);
+    return 0;
+}
+
+
