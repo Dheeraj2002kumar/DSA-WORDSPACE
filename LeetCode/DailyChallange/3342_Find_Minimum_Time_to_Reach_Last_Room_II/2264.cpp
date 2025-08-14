88336ae59a8f7b2df79c29d7fa5d57298b241389
Dheeraj Kumar <dk8549644@gmail.com>
2025-08-15 00:10:52 +0530

    added the solution of 2264

--- LeetCode/DailyChallange/3342_Find_Minimum_Time_to_Reach_Last_Room_II/2264.cpp
+++ LeetCode/DailyChallange/3342_Find_Minimum_Time_to_Reach_Last_Room_II/2264.cpp
@@ -43,3 +43,19 @@ num only consists of digits.
 
 */
 
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string maxgood = "";
+        for (int i = 0; i+2 < num.size(); i++){
+            if (num[i] == num[i+1] && num[i+1] == num[i + 2]){
+                string cur = num.substr(i, 3);
+
+                if (cur > maxgood){
+                    maxgood = cur;
+                }
+            }
+        }
+        return maxgood;
+    }
+};
