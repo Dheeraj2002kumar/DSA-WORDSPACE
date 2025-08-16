f1ba3d35b318055ed7f60934ca53a09954061ea2
Dheeraj Kumar <dk8549644@gmail.com>
2025-08-16 15:30:07 +0530

    added solution of two pointer sum

--- DSA/4_Two_pointer/two_pointer_sum_II_sorted_array.cpp
+++ DSA/4_Two_pointer/two_pointer_sum_II_sorted_array.cpp
@@ -42,3 +42,24 @@ numbers is sorted in non-decreasing order.
 The tests are generated such that there is exactly one solution.
 
 */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int>ans;
+        int start = 0, end = numbers.size()-1;
+
+        while(start < end){
+            if (numbers[start] + numbers[end] == target){
+                ans.push_back(start + 1);
+                ans.push_back(end + 1);
+                return ans;
+            } else if (numbers[start] + numbers[end] < target){
+                start++;
+            } else{
+                end--;
+            }
+        }
+        return ans;
+    }
+};
