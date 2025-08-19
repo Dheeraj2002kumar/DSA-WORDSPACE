45764f8075e3be0f04b0ae1b161c44ace94c0801
Dheeraj Kumar <dk8549644@gmail.com>
2025-08-19 22:10:54 +0530

    added solution of 2348 leetcode

--- LeetCode/DailyChallange/3342_Find_Minimum_Time_to_Reach_Last_Room_II/2348.cpp
+++ LeetCode/DailyChallange/3342_Find_Minimum_Time_to_Reach_Last_Room_II/2348.cpp
@@ -44,3 +44,45 @@ Constraints:
 
 */
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long result = 0;
+        int n = nums.size();
+        int i = 0;
+        
+        while(i < n){
+            long long l = 0;
+            if (nums[i] == 0){
+                while(i < n && nums[i] == 0){
+                    i++;
+                    l++;
+                }
+            } else{
+                i++;
+            }
+            result += (l)*(l+1)/2;
+        }
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    
+    // Example input
+    vector<int> nums = {0, 0, 1, 0, 0, 0};
+    
+    // Get the result
+    long long result = solution.zeroFilledSubarray(nums);
+    
+    // Output the result
+    cout << "Number of zero-filled subarrays: " << result << endl;
+
+    return 0;
+}
