0bc2397282b6882e8d1a95101509aa1696026008
Dheeraj Kumar <dk8549644@gmail.com>
2025-08-19 15:20:49 +0530

    Maximum value of in the column value in 2D array

--- DSA/8_2D_Array/2D_array.cpp
+++ DSA/8_2D_Array/2D_array.cpp
@@ -10,6 +10,24 @@ void printCal(int arr[][4], int row, int col){
     cout << endl;
 }
 
+void printRowMax(int arr[][4], int row, int col){
+    int index = -1, maxSum = INT_MIN;
+
+    for (int i = 0; i < row; i++){
+        int sum = 0; 
+        for(int j = 0; j < col; j++){
+            sum += arr[i][j];
+
+            if (sum > maxSum){
+                maxSum = sum;
+                index = i;
+            }
+        }
+    }
+    cout << "Maximum value of in the column: "<< maxSum << endl;
+    cout << "Index of the row: " << index << endl;
+}
+
 int main(){
     // create 2D array
     int arr[3][4] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
@@ -50,21 +68,27 @@ int main(){
 
     // add two matrix
 
-    for (int row = 0; row < 3; row++){
-        for (int col = 0; col < 4; col++){
-            sumOfTwoArray[row][col] = arr[row][col] + arr1[row][col];
-        }
-    }
+    // for (int row = 0; row < 3; row++){
+    //     for (int col = 0; col < 4; col++){
+    //         sumOfTwoArray[row][col] = arr[row][col] + arr1[row][col];
+    //     }
+    // }
 
-    // print sumOfTwoArray matrix
-    cout << "sumOfTwoArray matrix is:  " << endl;
-    
-    for (int row = 0; row < 3; row++){
-        for (int col = 0; col < 4; col++){
-            cout << sumOfTwoArray[row][col] << " ";
-        }
-    }
-    cout << endl;
+    // // print sumOfTwoArray matrix
+    // cout << "sumOfTwoArray matrix is:  " << endl;
+
+    // for (int row = 0; row < 3; row++){
+    //     for (int col = 0; col < 4; col++){
+    //         cout << sumOfTwoArray[row][col] << " ";
+    //     }
+    // }
+    // cout << endl;
+
+
+    // -------------------------------------------
+
+    // print maximum column index and value
+    printRowMax(arr, 3, 4);
 
     return 0;
 }
