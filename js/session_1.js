37eac405c33710e1bec8639c3aa7240641ae8e3b
Dheeraj Kumar <dk8549644@gmail.com>
2025-08-28 21:43:10 +0530

    session 1 js

---
+++ js/session_1.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = "abc";
+
+console.log("a + b is ", a+b)
+console.log("a - b is ", a-b)
+console.log("a * b is ", a*b)
+console.log("a / b is ", a/b)
+
+
+console.log("a + b typeof is ", typeof(a+b))
+console.log("a - b typeof is ", typeof(a-b))
+console.log("a * b typeof is ", typeof(a*b))
+console.log("a / b typeof is ", typeof(a/b))
+
