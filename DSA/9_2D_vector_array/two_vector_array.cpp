3c1af86746c701fe2a70b891ac5910816948c565
Dheeraj Kumar <dk8549644@gmail.com>
2025-08-30 23:35:39 +0530

    add valid-sudoku and two vector array file

--- DSA/9_2D_vector_array/two_vector_array.cpp
+++ DSA/9_2D_vector_array/two_vector_array.cpp
@@ -4,6 +4,24 @@
 
 using namespace std;
 
+void wave(int arr[][4], int row, int col){
+    for (int j = 0; j < col; j++){
+        // if even
+        if (j%2 == 0){
+            for (int i = 0; i < row; i++){
+                cout << arr[i][j] << " ";
+            }
+        }
+        // if odd
+        else{
+            for (int i = row-1; i >= 0; i--){
+                cout << arr[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
 int main(){
     // create 2d vector
     // vector<vector<int> > matrix(3, vector<int>(4, 1));
@@ -26,25 +44,34 @@ int main(){
 
     // take user input for 2D vector array
 
-    int n, m;
-    cout << "Enter rows and col: ";
-    cin >> n >> m;
-    vector<vector<int> > matrix(n, vector<int>(m, 1));
+    // int n, m;
+    // cout << "Enter rows and col: ";
+    // cin >> n >> m;
+    // vector<vector<int> > matrix(n, vector<int>(m, 1));
 
-    cout << "Enter the value for matrix: ";
+    // cout << "Enter the value for matrix: ";
     
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            cin >> matrix[i][j];
-        }
-    }
+    // for (int i = 0; i < n; i++){
+    //     for (int j = 0; j < m; j++){
+    //         cin >> matrix[i][j];
+    //     }
+    // }
 
-    for (int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+    // for (int i = 0; i < n; i++){
+    //     for(int j = 0; j < m; j++){
+    //         cout << matrix[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
 
+
+    // ----------------------------------------------
+
+    int arr1[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    int arr2[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int ans[3][4]; 
+
+    // wave print
+    wave(arr1, 3, 4);
     return 0;
 }
