74de9870c1eae0f6bae60db30b382bbb99eab66d
Dheeraj Kumar <dk8549644@gmail.com>
2026-03-07 23:31:14 +0530

    String operation

---
+++ DSA/11_String/String.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string s;
+    getline(cin, s); // getline can also read space
+    cout << s << endl;
+    cout << s.size();
+
+    return 0;
+}
+
+
+// -------------------------------
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     string s;
+//     cin >> s;
+//     cout << s;
+
+//     return 0;
+// }
+
+
+// ----------------------------------------
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     char arr[5];
+//     cin >> arr;
+//     arr[2] = '\0';
+//     cout << arr;
+
+//     return 0;
+// }
+
+
+
+
+
+// ---------------------------------------
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     // char arr[] = {'a', 'p', 'p', 'l', 'e'};
+
+//     // // cout << arr;
+//     // for (int i = 0; i < 5; i++){
+//     //     cout << arr[i] << endl;
+//     // }
+// }
